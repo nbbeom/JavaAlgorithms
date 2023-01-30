25b46f9dcbacc08b85792858575b28a508b7c603
nbbeom <5877177a@naver.com>
2023-01-30 23:37:21 +0900

    feat : Study 2023/01/30

--- algorithms/algorithms/src/main/java/Sort/Sorts.java
+++ algorithms/algorithms/src/main/java/Sort/Sorts.java
@@ -302,6 +302,52 @@ public class Sorts {
         System.out.println(ans);
 
     }
+    public void horse() throws IOException
+    {
+
+//        N개의 마구간이 수직선상에 있습니다. 각 마구간은 x1, x2, x3, ......, xN의 좌표를 가지며, 마구간간에 좌표가 중복되는 일은 없습니다.
+//        현수는 C마리의 말을 가지고 있는데, 이 말들은 서로 가까이 있는 것을 좋아하지 않습니다. 각 마구간에는 한 마리의 말만 넣을 수 있고,
+//        가장 가까운 두 말의 거리가 최대가 되게 말을 마구간에 배치하고 싶습니다.
+//         C마리의 말을 N개의 마구간에 배치했을 때 가장 가까운 두 말의 거리가 최대가 되는 그 최대값을 출력하는 프로그램을 작성하세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st1 = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st1.nextToken());
+        int m = Integer.parseInt(st1.nextToken());
+
+        int [] arrs = new int[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int lt = 0, rt = 0;
+        for (int i = 0; i < n; i++) {
+            arrs[i] = (Integer.parseInt(st.nextToken()));
+        }
+
+        int  ans =0 ;
+        Arrays.sort(arrs);
+        lt = 1;
+        rt = arrs[n-1];
+        while(lt<=rt){
+            int mid = (lt+rt)/2;
+            if(count(arrs,mid) >= m){
+                ans = mid;
+                lt = mid+1;
+            }else{
+                rt = mid -1;
+            }
+        }
+        System.out.println(ans);
+    }
+    public static int count(int[] arrs , int dist){
+        int cnt =1;
+        int ep = arrs[0];
+        for(int i=1;i< arrs.length;i++){
+            if(arrs[i]-ep >= dist){
+                cnt ++;
+                ep =arrs[i];
+            }
+        }
+        return cnt;
+    }
 
 
 }
